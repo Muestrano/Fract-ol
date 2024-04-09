6ce07c7493b4bd2a6304558b26ecb59c817c581f
Pierre Carlier <picarlie@c3r1p2.42perpignan.fr>
2024-04-09 16:52:35 +0200

    Test

---
+++ fractol.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: picarlie <picarlie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/02 17:06:23 by picarlie          #+#    #+#             */
+/*   Updated: 2024/04/09 16:13:46 by picarlie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <math.h>
+#include "minilibx-linux/mlx.h"
+
+#define ERROR_MESSAGE "Please enter \n\t\"./fractol mandelbrot\" or \n\t\"./fractol julia <value_1> <value_2>\"\n"
+
+#endif
